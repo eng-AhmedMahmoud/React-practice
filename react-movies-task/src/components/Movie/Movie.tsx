ea41bad93d7fa39453950beffbb057a8bf5fea56
Ahmed Mahmoud <ahmeddahabmohamed@gmail.com>
2023-11-15 14:44:27 +0200

    Second Version Release

--- react-movies-task/src/components/Movie/Movie.tsx
+++ react-movies-task/src/components/Movie/Movie.tsx
@@ -1,21 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Movie.module.scss";
+import { MovieInterface } from "../../interfaces/MovieInterface";
 
-const Movie = (props: any) => {
-    const movie = { ...props.movie };
-    return (
-        <div className={styles.card} key={movie.id}>
-            <img
-                className={styles["card-image"]}
-                src={movie.image}
-                alt={movie.title}
-            />
+interface MovieProps {
+  movie: MovieInterface;
+  handleDelete: (id: number) => void;
+  handleUpdate: (id: number, updatedMovie: MovieInterface) => void;
+}
+
+const Movie = ({ movie, handleDelete, handleUpdate }: MovieProps) => {
+  const { id, title, description, image } = movie;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedMovie, setUpdatedMovie] = useState(movie);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setUpdatedMovie(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleUpdate(id, updatedMovie);
+    setIsEditing(false);
+  };
+
+  return (
+      <div className={styles.card} key={id}>
+          <img className={styles["card-image"]} src={image} alt={title} />
           <div>
-              <h3 className={styles["card-title"]}> {movie.title}</h3>
-              <p className={styles["card-description"]}> {movie.description}</p>
+              {isEditing ? (
+                  <form className={styles["form"]} onSubmit={handleFormSubmit}>
+                      <label className={styles["form-label"]}>
+                          Title:
+                          <input
+                              className={styles["form-input"]}
+                              type="text"
+                              name="title"
+                              value={updatedMovie.title}
+                              onChange={handleInputChange}
+                          />
+                      </label>
+                      <label className={styles["form-label"]}>
+                          <br />
+                          Description:
+                          <br />
+                          <textarea
+                              className={styles["form-textarea"]}
+                              name="description"
+                              value={updatedMovie.description}
+                              onChange={handleInputChange}
+                          />
+                      </label>
+                      <br />
+                      <button type="submit">Save</button> &nbsp;
+                      <button type="button" onClick={() => setIsEditing(false)}>
+                          Cancel
+                      </button>
+                  </form>
+              ) : (
+                  <>
+                      <h3 className={styles["card-title"]}>{title}</h3>
+                      <p className={styles["card-description"]}>
+                          {description}
+                      </p>
+                      <button
+                          className={styles["button-delete"]}
+                          onClick={() => handleDelete(id)}
+                      >
+                          Delete
+                      </button>
+                      <button
+                          className={styles["button-update"]}
+                          onClick={() => setIsEditing(true)}
+                      >
+                          Update
+                      </button>
+                  </>
+              )}
           </div>
-        </div>
-    );
+      </div>
+  );
 };
 
 export default Movie;
--- react-movies-task/src/components/Movies/Movies.tsx
+++ react-movies-task/src/components/Movies/Movies.tsx
@@ -1,16 +1,40 @@
-import React from "react";
-import movies from "../../Data/MoviesData";
+import React, { useState } from "react";
+import moviesData from "../../Data/MoviesData";
 import Movie from "../Movie/Movie";
 import styles from "./Movies.module.scss";
 
 const Movies = () => {
-    return (
-        <div className={styles.container}>
-            {movies.map((movie) => (
-                <Movie key={movie.id} movie={movie} />
-            ))}
-        </div>
-    );
+  const [movies, setMovies] = useState([...moviesData]);
+
+  const handleDelete = (id: number) => {
+    setMovies(movies.filter((movie) => movie.id !== id));
+  };
+
+  const handleUpdate = (id: number, updatedMovie: any) => {
+    setMovies(movies.map((movie) => {
+      if (movie.id === id) {
+        return {
+          ...movie,
+          ...updatedMovie
+        };
+      } else {
+        return movie;
+      }
+    }));
+  };
+
+  return (
+    <div className={styles.container}>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          movie={movie}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
